6b49d607e0d842ff56c21ca826bc551f4d3134b8
LAPTOP-CTB3VCJK\zpw151034 <1762910894@qq.com>
2021-07-27 13:19:06 +0800

    '客服消息'

---
+++ Basics/WxConstant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HugCode\WeChat\Basics;
+
+class WxConstant
+{
+
+    // 消息类型
+    const MS_TYPE_MP_NEWS = 'mpnews';  // 图文消息
+    const MS_TYPE_TEXT = 'text';  // 文本消息
+    const MS_TYPE_VOICE = 'voice';  // 语音消息
+    const MS_TYPE_MUSIC = 'music';  // 音乐
+    const MS_TYPE_IMAGE = 'image';  // 图片
+    const MS_TYPE_VIDEO = 'video';  // 视频
+    const MS_TYPE_WX_CARD = 'wxcard';  // 卡券
+    const MS_TYPE_NEWS = 'news'; // 图文消息
+    const MS_TYPE_WX_APP = 'miniprogrampage'; // 小程序
+
+}
--- MessageManagement/ServiceCenterMessages.php
+++ MessageManagement/ServiceCenterMessages.php
@@ -3,27 +3,27 @@
 namespace HugCode\WeChat\MessageManagement;
 
 use HugCode\WeChat\Basics\WxConstant;
-use HugCode\WeChat\Basics\HttpRequest;
 use HugCode\WeChat\Basics\BasicWeChat;
+use HugCode\WeChat\Basics\HttpRequest;
 use HugCode\WeChat\Basics\Exception\ErrorCode;
 use HugCode\WeChat\Basics\Exception\MessageException;
 
-class BatchSends extends BasicWeChat
+class ServiceCenterMessages extends BasicWeChat
 {
 
-    const SEND = 'message/mass/send'; // 根据OpenID列表群发【订阅号不可用，服务号认证后可用】
+    const SEND = 'message/custom/send'; // 客服接口-发消息
 
     /**
-     * @Desc 根据OpenID列表群发【订阅号不可用，服务号认证后可用】
+     * @Desc 客服接口-发消息
      * @param array $data
      * @return mixed
      * @throws MessageException
-     * @author hug-code
-     * @Time 2021/7/26 16:57
+     * @author yashuai
+     * @Time 2021/7/27 11:11
      */
     public function send($data = [])
     {
-        if (!isset($data['touser']) || empty($data['touser']) || !is_array($data['touser'])) {
+        if (!isset($data['touser']) || empty($data['touser']) || !is_string($data['touser'])) {
             throw new MessageException(ErrorCode::PARAMS_ERROR_TO_USER_EMPTY);
         }
         $msgType = [
@@ -33,17 +33,13 @@ class BatchSends extends BasicWeChat
             WxConstant::MS_TYPE_MUSIC,
             WxConstant::MS_TYPE_IMAGE,
             WxConstant::MS_TYPE_VIDEO,
-            WxConstant::MS_TYPE_WX_CARD
+            WxConstant::MS_TYPE_WX_CARD,
+            WxConstant::MS_TYPE_NEWS,
+            WxConstant::MS_TYPE_WX_APP,
         ];
         if (!isset($data['msgtype']) || !in_array($data['msgtype'], $msgType)) {
             throw new MessageException(ErrorCode::PARAMS_ERROR_MSG_TYPE_ERROR);
         }
-
-        $toUserCount = count($data['touser']);
-        if ($toUserCount < 2 || $toUserCount > 10000) {
-            throw new MessageException(ErrorCode::PARAMS_ERROR_TO_USER_ERROR);
-        }
-
         $url = self::API_BASE_URL . self::SEND . '?access_token=' . $this->access_token;
         return HttpRequest::instance()->post($url, json_encode($data, JSON_UNESCAPED_UNICODE))->toArray();
     }
